8b0a4d37d48431d980a624be6d0b2db7301a7d18
Janez Križaj <jkrizaj@hpc-login1.arnes.si>
2024-01-24 21:24:37 +0100

    changed venv name

--- train_singlenode_dp.sh
+++ train_singlenode_dp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --job-name=train_singlenode_dp
-#SBATCH --output=train_singlenode.out
+#SBATCH --output=train_singlenode_dp.out
 #SBATCH --time=01:00:00       # job time limit
 #SBATCH --nodes=1             # number of nodes
 #SBATCH --ntasks-per-node=1   # number of tasks
@@ -10,9 +10,9 @@
 #SBATCH --mem-per-gpu=32G     # memory allocation
 
 source ~/miniconda3/etc/profile.d/conda.sh # intialize conda
-conda activate py310                 # activate the previously created environment
+conda activate pytorch_env                 # activate the previously created environment
 
 OUT_PATH=/d/hpc/projects/FRI/DL/example/bird_data/
 
 # Run the training script twice with different hyperparameters.
-srun --nodes=1 --gpus=2 --ntasks=1 python train_singlenode_dp.py --lr 1e-4 --epochs 10 --batch_size 128 --out_path $OUT_PATH
+srun --nodes=1 --gpus=2 --ntasks=1 python train_singlenode_dp.py --lr 1e-4 --epochs 4 --batch_size 128 --out_path $OUT_PATH
