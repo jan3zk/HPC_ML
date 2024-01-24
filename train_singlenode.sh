9459daaba945d201ccddf4288f5b3f80ebd14712
Janez Križaj <jkrizaj@hpc-login1.arnes.si>
2024-01-24 19:33:51 +0100

    updated sbatch

--- train_singlenode.sh
+++ train_singlenode.sh
@@ -11,12 +11,12 @@
 #SBATCH --mem-per-gpu=32G     # memory allocation
 
 source ~/miniconda3/etc/profile.d/conda.sh  # initialize conda
-conda activate py310  # activate the previously created environment
+conda activate pytorch_env  # activate the previously created environment
 
 OUT_PATH=/d/hpc/projects/FRI/DL/example/bird_data/
 
 # Run training twice using different hyperparameters
 srun --nodes=1 --exclusive --gpus=1 --ntasks=1 python train_singlenode.py --gpu 0 --lr 1e-4 --epochs 4 --batch_size 64 --out_path $OUT_PATH &
-srun --nodes=1 --exclusive --gpus=1 --ntasks=1 python train_singlenode.py --gpu 1 --lr 1e-3 --epochs 4 --batch_size 64 --out_path $OUT_PATH &
+srun --nodes=1 --exclusive --gpus=1 --ntasks=1 python train_singlenode.py --gpu 0 --lr 1e-3 --epochs 4 --batch_size 64 --out_path $OUT_PATH &
 
 wait
