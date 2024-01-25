0e33d81cbaa010bfae6f0e0245fc937557c0ce2d
Janez Križaj <jkrizaj@hpc-login2.arnes.si>
2024-01-25 11:50:01 +0100

    corrected comment

--- train_singlenode_dp.sh
+++ train_singlenode_dp.sh
@@ -14,5 +14,5 @@ conda activate pytorch_env                 # activate the previously created env
 
 OUT_PATH=/d/hpc/projects/FRI/DL/example/bird_data/
 
-# Run the training script twice with different hyperparameters.
+# Run the training script
 srun --nodes=1 --gpus=2 --ntasks=1 python train_singlenode_dp.py --lr 1e-4 --epochs 4 --batch_size 128 --out_path $OUT_PATH
