640bf19d2b30b3e6e1ec300a37cb5be50cc48ec8
Janez Križaj <jkrizaj@hpc-login2.arnes.si>
2024-07-04 19:03:52 +0200

    Simplified DDP scripts.

--- train_multinode_ddp.py
+++ train_multinode_ddp.py
@@ -1,9 +1,8 @@
-import os, sys
+import os
 import argparse
 import torch
 from torch import nn
 import numpy as np
-import random
 import torch.distributed as dist
 import torchvision
 import torchvision.transforms as transforms
@@ -13,43 +12,25 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
     parser.add_argument('--batch_size', default=16, type=int, help='batch size per GPU')
-    parser.add_argument('--gpu', default=None, type=int)
-    parser.add_argument('--start_epoch', default=0, type=int,
-                        help='start epoch number (useful on restarts)')
     parser.add_argument('--epochs', default=10, type=int, help='number of total epochs to run')
     parser.add_argument('-j', '--workers', default=12, type=int, metavar='N',
                         help='number of data loading workers (default: 32)')
     # DDP configs:
-    parser.add_argument('--world_size', default=-1, type=int,
-                        help='number of nodes for distributed training')
-    parser.add_argument('--rank', default=-1, type=int,
-                        help='node rank for distributed training')
     parser.add_argument('--dist_url', default='env://', type=str,
                         help='url used to set up distributed training')
     parser.add_argument('--dist_backend', default='nccl', type=str,
                         help='distributed backend')
-    parser.add_argument('--local_rank', default=-1, type=int,
-                        help='local rank for distributed training')
     parser.add_argument('--out_path', default='./bird_data/', type=str)
     args = parser.parse_args()
     return args
 
-def main(args):
-    # DDP setting
-    if "WORLD_SIZE" in os.environ:
-        args.world_size = int(os.environ["WORLD_SIZE"])
-    args.distributed = args.world_size > 1
-    ngpus_per_node = torch.cuda.device_count()
+WORLD_SIZE = int(os.environ['SLURM_NTASKS'])
+WORLD_RANK = int(os.environ['SLURM_PROCID'])
+LOCAL_RANK =int(os.environ['SLURM_LOCALID'])
 
-    if args.distributed:
-        if args.local_rank != -1:  # for torch.distributed.launch
-            args.rank = args.local_rank
-            args.gpu = args.local_rank
-        elif 'SLURM_PROCID' in os.environ:  # for slurm scheduler
-            args.rank = int(os.environ['SLURM_PROCID'])
-            args.gpu = args.rank % torch.cuda.device_count()
-        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
-                                world_size=args.world_size, rank=args.rank)
+def main(args):
+    dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                            world_size=WORLD_SIZE, rank=WORLD_RANK)
 
     ### model ###
     model = torchvision.models.resnet152(weights=torchvision.models.ResNet152_Weights.IMAGENET1K_V1)
@@ -59,29 +40,17 @@ def main(args):
     for param in model.fc.parameters():
         param.requires_grad = True
 
-    if args.distributed:
-        # For multiprocessing distributed, DistributedDataParallel constructor
-        # should always set the single device scope, otherwise,
-        # DistributedDataParallel will use all available devices.
-        if args.gpu is not None:
-            torch.cuda.set_device(args.gpu)
-            model.cuda(args.gpu)
-            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-            model_without_ddp = model.module
-        else:
-            model.cuda()
-            model = torch.nn.parallel.DistributedDataParallel(model)
-            model_without_ddp = model.module
-    else:
-        raise NotImplementedError("Only DistributedDataParallel is supported.")
-
+    torch.cuda.set_device(LOCAL_RANK)
+    model.cuda(LOCAL_RANK)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[LOCAL_RANK])
+    
     wandb.config = {
         "learning_rate": args.lr,
         "epochs": args.epochs,
         "batch_size": args.batch_size
     }
 
-    if args.rank == 0:
+    if WORLD_RANK == 0:
         wandb.init(mode="disabled", project="bird_example",
                    entity=os.environ["USER"], name="bird_example_multinode_ddp")
 
@@ -108,47 +77,41 @@ def main(args):
     criterion = torch.nn.CrossEntropyLoss()
 
     ### main loop ###
-    for epoch in range(args.start_epoch, args.epochs):
-        np.random.seed(epoch)
-        random.seed(epoch)
-        if args.distributed:
-            train_loader.sampler.set_epoch(epoch)
+    for epoch in range(args.epochs):
+        train_one_epoch(train_loader, model, criterion, optimizer, epoch)
+        validate(val_loader, model, criterion, epoch)
 
-        train_one_epoch(train_loader, model, criterion, optimizer, epoch, args)
-        validate(val_loader, model, criterion, epoch, args)
-
-def train_one_epoch(train_loader, model, criterion, optimizer, epoch, args):
+def train_one_epoch(train_loader, model, criterion, optimizer, epoch):
     model.train()
-    print(f"Epoch {epoch}, Rank {args.rank}")
     for i,batch in enumerate(train_loader):
-        image_batch = batch[0].cuda(args.gpu, non_blocking=True)
-        annotation_batch = batch[1].cuda(args.gpu, non_blocking=True)
+        image_batch = batch[0].cuda(LOCAL_RANK, non_blocking=True)
+        annotation_batch = batch[1].cuda(LOCAL_RANK, non_blocking=True)
         output = model(image_batch)
         loss = criterion(output, annotation_batch)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-def validate(val_loader, model, criterion, epoch, args):
-    counter = torch.zeros((2,), device=torch.device(f'cuda:{args.gpu}'))
+def validate(val_loader, model, criterion, epoch):
+    counter = torch.zeros((2,), device=torch.device(f'cuda:{LOCAL_RANK}'))
     model.eval()
     first_batch_images = None
     first_batch_predictions = None
     first_batch_gt = None
     for i,batch in enumerate(val_loader):
-        image_batch = batch[0].cuda(args.gpu, non_blocking=True)
-        annotation_batch = batch[1].cuda(args.gpu, non_blocking=True)
+        image_batch = batch[0].cuda(LOCAL_RANK, non_blocking=True)
+        annotation_batch = batch[1].cuda(LOCAL_RANK, non_blocking=True)
         output = model(image_batch)
         labels = torch.argmax(output, dim=1)
         counter[0] += torch.sum((labels == annotation_batch).float())
         counter[1] += image_batch.shape[0]
-        if args.rank == 0 and i==0:
+        if WORLD_RANK == 0 and i==0:
             first_batch_images = image_batch
             first_batch_predictions = labels
             first_batch_gt = annotation_batch
 
     dist.reduce(counter, 0)
-    if args.rank == 0:
+    if WORLD_RANK == 0:
         pred_table = wandb.Table(columns=["Image", "Prediction", "GT"])
         for j in range(first_batch_images.shape[0]):
             pred_class_name = val_loader.dataset.classes[first_batch_predictions[j].item()]
@@ -162,3 +125,4 @@ def validate(val_loader, model, criterion, epoch, args):
 if __name__ == '__main__':
     args = parse_args()
     main(args)
+
